1e1525e18c109ad0447082cfd65a4e0061e79972
Alireza-Torabi <133779215+Alireza-Torabi@users.noreply.github.com>
2023-11-14 16:43:04 +0330

    Update AddRootPrivilegeUser.sh

--- AddRootPrivilegeUser.sh
+++ AddRootPrivilegeUser.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Prompt for username
+read -p "Enter the username: " username
+
+# Prompt for password (Note: This is not secure for production use)
+read -s -p "Enter the password: " password
+echo
+
+# Create user with root privileges
+sudo useradd -m -s /bin/bash $username
+echo "$username:$password" | sudo chpasswd
+
+# Add user to the sudo group
+sudo usermod -aG sudo $username
+
+echo "User '$username' created with root privileges."

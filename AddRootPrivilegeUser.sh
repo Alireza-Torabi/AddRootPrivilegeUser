c58208a6a28bfb7c4744beaf32de7c8619923cd1
Alireza-Torabi <133779215+Alireza-Torabi@users.noreply.github.com>
2023-11-16 15:30:22 +0330

    Update AddRootPrivilegeUser.sh
    change checks user exist or not

--- AddRootPrivilegeUser.sh
+++ AddRootPrivilegeUser.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Check if the script is run with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root."
+  exit 1
+fi
+
 # Prompt for username
 read -p "Enter the username: " username
+
+# Check if the user already exists
+if id "$username" &>/dev/null; then
+  echo "User '$username' already exists."
+  exit 1
+fi
 
 # Prompt for password (Note: This is not secure for production use)
 read -s -p "Enter the password: " password
